c14aa3e75116ea5b1ac0c2a75e08be482708c9f7
Giovanne Pinto <pintogiovanne@gmail.com>
2021-10-24 21:05:54 -0400

    Edited files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-/* Add your JavaScript to this file */
+/* Add your JavaScript to this file */ 
+window.onload=function(){
+    let submit = document.getElementsByClassName("btn");
+    let form = document.getElementById("email");
+    let msg = document.getElementsByClassName("message");
+
+    submit[1].addEventListener("click",function(event){
+        event.preventDefault();
+
+        if (form.value !="" && form.value.includes("@")){
+            msg[0].innerHTML = "Thank you! Your email address " + form.value +" has been added to out mailing list!"
+        }else{
+            msg[0].innerHTML = "Please enter valid email address" 
+        }
+    })
+}
